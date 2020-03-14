712ac2a5a0a4a8b9a4615d51a2be608c2a208d6b
Cole Davis <colebuildanddevelop@gmail.com>
2020-03-14 20:00:15 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,8 +2,10 @@
 
 def map_to_negativize(source_array)
   index = 0
+  array = []
   while source_array[index] do
-    
+    array << (source_array[index] * -1)
     index += 1 
   end 
+  array
 end
